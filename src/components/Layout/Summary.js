ce1d524bbda56483dd2b5bab8a54d936f9bc8053
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-06-28 16:22:48 +0530

    Summary-box

---
+++ src/components/Layout/Summary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Summary.css';
+
+const Summary = () => {
+    return(
+        <>
+        <div className='container'>
+            <h1 className='text-summary-heading'>Delicious Food, Delivered To You</h1>
+            <div className='text-summary-1'>
+                <p>Choose your favaorite meal from our broad selection of avaliable meals and enjoy a delicious <br />
+                <span>lunch or dinner at home</span></p>
+                <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by <br />
+                <span>experienced chefs!</span></p>
+            </div>
+            
+        </div>
+        </>
+    )
+}
+
+export default Summary;
