23e46edc608a46aabdc31a3ef3634db735c723e0
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-06-28 15:44:03 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <>
+        <nav>
+            <div className='heading'>
+                <h1 className='heading-1'>ReactMeals</h1>
+            </div>
+            <div className='cart-button'>
+                <button type='button' className='btn-1'>Your Cart <span>0</span></button>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+
+export default Header;
