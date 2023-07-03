77f9ccaa8bf43d218d929645da1fc9d841210eec
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-07-03 15:08:26 +0530

    Cart items page and minus button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/CartItems';
+import Cart from './components/Cart/Cart';
 import CartProvider from './Store/CartProvider';
 
 function App() {
